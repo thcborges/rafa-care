ac3b5ac5f95c920c8c68bdb057fa33e9c08c45a0
Thiago da Cunha Borges <thiagoborges@id.uff.br>
2021-07-06 03:04:22 -0300

    Models to save breastfeeding

---
+++ rafa_care/ext/models/__init__.py
@@ -0,0 +1,3 @@
+from rafa_care.ext.models.diaper_changes import DiaperChange
+from rafa_care.ext.models.milk_sources import MilkSource
+from rafa_care.ext.models.breastfeeding import Breastfeeding
---
+++ rafa_care/ext/models/milk_sources.py
@@ -0,0 +1,17 @@
+from rafa_care.ext.db import db
+
+
+class MilkSource(db.Model):
+    __tablename__ = "milk_sources"
+    id = db.Column("id", db.Integer, primary_key=True, autoincrement=True)
+    description = db.Column("Description",
+                            db.Unicode(50),
+                            nullable=False,
+                            unique=True)
+    created_at = db.Column("created_at",
+                           db.DateTime(),
+                           server_default=db.func.now())
+    updated_at = db.Column("updated_at",
+                           db.DateTime(),
+                           server_default=db.func.now(),
+                           onupdate=db.func.now())
